1e4cc6d17be016b7d31a8036c8fbc2873842c0e8
ChantellDavis <155310121+ChantellDavis@users.noreply.github.com>
2025-08-14 18:16:13 -0400

    added js file

---
+++ src/index.js
@@ -0,0 +1,15 @@
+let cardOne = 10
+let cardTwo = 11
+let sum = cardOne + cardTwo
+
+let cardTitle = document.getElementById("card-title")
+let sumTitle = document.getElementById("sum-title")
+let mainQuestion = document.getElementById("main-question")
+
+// function start() {
+    
+// } 
+
+if (sum < 21) {
+    console.log = "You can pick up more cards"
+}
